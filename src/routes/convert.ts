c8f072a808b4153189f12a8b68dd2e936c858eac
Will Parks <wparks08@gmail.com>
2020-06-24 11:08:06 -0700

    fix: remove JSESSIONID dependency from convert route

--- src/routes/convert.ts
+++ src/routes/convert.ts
@@ -6,29 +6,22 @@ import { body, cookie, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post(
-    "/convert",
-    [
-        body("url", "url missing or invalid").exists(),
-        cookie("JSESSIONID", "session id not found - must be logged in").exists(),
-    ],
-    async (req: Request, res: Response) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            console.log(errors);
-            throw new ValidationError(errors.array());
-        }
-
-        const { url } = req.body;
-
-        try {
-            const pdf = await PDFService.getPDF(url, req.cookies);
-            res.status(200).set("Content-Type", "application/pdf").send(pdf);
-        } catch (e) {
-            console.log(e.stack);
-            throw new PDFCreationError(e.message);
-        }
+router.post("/convert", [body("url", "url missing or invalid").exists()], async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log(errors);
+        throw new ValidationError(errors.array());
     }
-);
+
+    const { url } = req.body;
+
+    try {
+        const pdf = await PDFService.getPDF(url, req.cookies);
+        res.status(200).set("Content-Type", "application/pdf").send(pdf);
+    } catch (e) {
+        console.log(e.stack);
+        throw new PDFCreationError(e.message);
+    }
+});
 
 export { router as convertRouter };
